1dacceb0f5dc6793fd205c553cbf356f1f809610
KaizenG <desiigner4074@gmail.com>
2025-08-21 21:15:39 +0800

    for setup purposes

---
+++ genbuild-billing-system/src/main/java/gino/desiigner/genbuild_billing_system/GenbuildBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package gino.desiigner.genbuild_billing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenbuildBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenbuildBillingSystemApplication.class, args);
+	}
+
+}
---
+++ genbuild-billing-system/src/test/java/gino/desiigner/genbuild_billing_system/GenbuildBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package gino.desiigner.genbuild_billing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenbuildBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
